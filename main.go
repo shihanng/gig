b9190b0179bdde8ea34eec62c0b58d599baaeea9
Shi Han NG <shihanng@gmail.com>
2019-11-26 21:34:49 +0900

    Add orderer

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 
 	"gopkg.in/src-d/go-git.v4"
 )
@@ -110,4 +112,67 @@ func readOrder(path string) (map[string]int, error) {
 
 func isComment(line string) bool {
 	return line != "" && line[0] == '#'
+}
+
+type template struct {
+	name  string
+	type_ string
+}
+
+type orderer struct {
+	templates []template
+	special   map[string]int
+}
+
+func (o *orderer) Len() int {
+	return len(o.templates)
+}
+
+func (o *orderer) Swap(i, j int) {
+	o.templates[i], o.templates[j] = o.templates[j], o.templates[i]
+}
+
+func (o *orderer) Less(i, j int) bool {
+	for _, lessFn := range []func(int, int) bool{
+		o.lessSpecial,
+	} {
+		less := lessFn
+		switch {
+		case less(i, j):
+			return true
+		case less(j, i):
+			return false
+		}
+	}
+	return o.lessName(i, j)
+}
+
+func (o *orderer) lessSpecial(i, j int) bool {
+	in, jn := canon(o.templates[i].name), canon(o.templates[j].name)
+
+	io, ok := o.special[in]
+	if !ok {
+		return false
+	}
+
+	jo, ok := o.special[jn]
+	if !ok {
+		return false
+	}
+
+	return io < jo
+}
+
+func (o *orderer) lessName(i, j int) bool {
+	in, jn := canon(o.templates[i].name), canon(o.templates[j].name)
+	return in < jn
+}
+
+func Sort(o orderer) orderer {
+	sort.Sort(&o)
+	return o
+}
+
+func canon(v string) string {
+	return strings.ToLower(v)
 }
